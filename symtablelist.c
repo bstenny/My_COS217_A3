6c53c8c3c05f04d8232d81f5c1e06cedbf1bf985
Blake Stenstrom <bns@princeton.edu>
2022-03-22 14:58:41 -0400

    TEsting

--- symtablelist.c
+++ symtablelist.c
@@ -163,7 +163,6 @@ void *SymTable_remove(SymTable_T oSymTable, const char *pcKey) {
     struct symTableNode *nextNode;
     void *value;
     assert(oSymTable != NULL);
-    assert(oSymTable->psFirstNode != NULL);
     psNewNode = (struct symTableNode*)malloc(sizeof(struct symTableNode));
     if (psNewNode == NULL) {
         return NULL;
